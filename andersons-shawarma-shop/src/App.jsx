36c9162e129aa2b85a478c07152c3014c4b9cedd
Anderson Caleb <dmankellz@gmail.com>
2025-10-06 22:25:20 +0100

    Navbar added

--- andersons-shawarma-shop/src/App.jsx
+++ andersons-shawarma-shop/src/App.jsx
@@ -1,11 +1,83 @@
+import { useState } from "react";
+import React from "react";
+import Hero from "./components/Hero";
+import logo from "/public/images/logo.png";
+import { LuShoppingBag } from "react-icons/lu";
+import { CiMenuBurger } from "react-icons/ci";
+import { GrClose } from "react-icons/gr";
+
 function App() {
+  const [menu, setMenu] = useState(false);
+  const handleChange = () => {
+    setMenu(!menu);
+  };
   return (
     <>
-      <div className="bg-[#1F2629]">
-        <h1 className="font-night text-[#D9D9D9] text-4xl">
-          Anderson's Shawarma Shop
-        </h1>
-        <p className="font-manrope text-base">This is a paragraph.</p>
+      <div className="min-h-screen flex flex-col bg-[#1F2629]">
+        <header className="px-18 md:px-32 py-4 flex items-center justify-between shadow-xl">
+          <div className="">
+            <img src={logo} alt="Logo" />
+          </div>
+          <nav className="space-x-6 hidden text-lg font-manrope font-semibold text-[#D9D9D9] md:flex">
+            <a href="#" className="hover:text-green-400 transition-all">
+              Home
+            </a>
+            <a href="#about" className="hover:text-green-400 transition-all">
+              About
+            </a>
+            <a href="#menu" className="hover:text-green-400 transition-all">
+              Menu
+            </a>
+            <a href="#Contact" className="hover:text-green-400 transition-all">
+              Contact Us
+            </a>
+          </nav>
+
+          <div className="flex items-center font-semibold space-x-8">
+            <button className="hidden md:flex text-[#D9D9D9] hover:text-green-400 text-3xl">
+              <LuShoppingBag />
+            </button>
+            <button className="hidden md:flex bg-transparent hover:text-[#D9D9D9] text-lg font-semibold font-manrope text-[#D9D9D9] border border-[#D9D9D9]  hover:bg-green-400 transition-all px-16 py-3 rounded">
+              Log In
+            </button>
+          </div>
+          <div className="md:hidden flex items-center text-[#D9D9D9]">
+            {menu ? (
+              <GrClose size={30} onClick={handleChange} />
+            ) : (
+              <CiMenuBurger size={30} onClick={handleChange} />
+            )}
+          </div>
+          <div
+            className={` ${
+              menu ? "translate-x-0" : "-translate-x-full"
+            } lg:hidden flex flex-col absolute bg-[#D9D9D9] text-[#1F2629] left-0 top-20 font-semibold text-2xl text-center pt-8 pb-4 gap-8 w-full h-fit transition-transform duration-300`}
+          >
+            <nav className="space-x-6 text-lg font-manrope font-semibold text-[#D9D9D9]">
+              <a href="#" className="hover:underline transition">
+                Home
+              </a>
+              <a href="#about" className="hover:underline">
+                About
+              </a>
+              <a href="#menu" className="hover:underline">
+                Menu
+              </a>
+              <a href="#Contact" className="hover:underline">
+                Contact Us
+              </a>
+            </nav>
+
+            <div className="font-semibold space-x-8">
+              <button className="bg-transparent  hover:text-[#D9D9D9] text-lg font-semibold font-manrope text-[#D9D9D9] border border-[#D9D9D9] hover:bg-[#D9D9D9] px-16 py-3 rounded">
+                Log In
+              </button>
+            </div>
+          </div>
+        </header>
+        <main className="flex-1">
+          <Hero />
+        </main>
       </div>
     </>
   );
---
+++ andersons-shawarma-shop/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <>
+      <section className="relative">
+        <div>
+          <h1 className="font-night text-[#D9D9D9] ">WHO WE ARE</h1>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Hero;
